d46d396338b07514703296d7d7c46c8099062cd6
Raihanbujeee <bujexixi@gmail.com>
2026-02-23 21:15:56 +0700

    perubahan praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
+Route::get('/world', function () {
+ return 'World';
+});
 
 Route::get('/', function () {
-    return view('welcome');
+ return 'Selamat datang';
+});
+
+Route::get('/about', function () {
+ return 'Nim:244107020087 <br> 
+ Nama:Raihan Akbar Putra Prasetyo' ;
+});
+
+Route::get('/user/{name}', function ($name) {
+return 'Nama saya '.$name;
 });
